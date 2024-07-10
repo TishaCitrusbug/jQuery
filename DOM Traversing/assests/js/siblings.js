29dfe8eb19705c430024f2693a818abcb224f718
tisa maheta <tisha.citrusbug@gmail.com>
2024-07-10 00:45:51 -1100

    add all changes

---
+++ DOM Traversing/assests/js/siblings.js
@@ -0,0 +1,13 @@
+// Sibling Methods () :- siblings(), next(), nextAll(), nextUntil(), prev(), prevAll(), prevUntil()
+
+$(document).ready(function(){
+
+    $('#child-c').siblings().css('background','black'); //The siblings() method returns all sibling elements of the selected element.
+    //$('#child-c').next().css('background','black'); //The next() method returns the next sibling element of the selected element.
+    //$('#child-c').nextAll().css('background','black'); //The nextAll() method returns all next sibling elements of the selected element.
+   //$('#child-c').nextUntil('.test').css('background','black'); // The nextUntil() method returns all next sibling elements between two given arguments.
+   // $('#child-c').prev().css('background','black'); 
+   // $('#child-c').prevAll().css('background','black');
+   // $('#child-c').prevUntil('.test').css('background','black');
+    
+});
