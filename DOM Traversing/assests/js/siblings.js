2fd85896b00f9aec03332184efecc9bf5a9b416d
tisa maheta <tisha.citrusbug@gmail.com>
2024-07-10 19:52:28 -1100

    add ajax file and folder

--- DOM Traversing/assests/js/siblings.js
+++ DOM Traversing/assests/js/siblings.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-    $('#child-c~#abcd').css('background','black'); //The siblings() method returns all sibling elements of the selected element.
+    $('#child-c~#abcd').css('background','black'); //The siblings() method returns all sibling elements of the selected element. ~ this sign used for siblings from one parent 
     //$('#child-c').next().css('background','black'); //The next() method returns the next sibling element of the selected element.
     //$('#child-c').nextAll().css('background','black'); //The nextAll() method returns all next sibling elements of the selected element.
    //$('#child-c').nextUntil('.test').css('background','black'); // The nextUntil() method returns all next sibling elements between two given arguments.
